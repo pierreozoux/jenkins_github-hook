a8ec017a16db2eaf44ac19ae17d11afaf21c9694
pierre-o13 <pierre.ozoux@seedrs.com>
2013-03-12 11:50:12 +0000

    add depends in the metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of jenkins_github-hook.
 
+## 0.1.1:
+
+* add depends on the metadata - nginx and jenkins
+
 ## 0.1.0:
 
 * Initial release of jenkins_github-hook
--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ maintainer_email 'pierre.ozoux@gmail.com'
 license          'WTFPL'
 description      'Installs/Configures jenkins_github-hook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+
+depends "jenkins"
+depends "nginx"
