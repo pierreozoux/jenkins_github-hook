ed4366d44b443fe079bf428e6e558680615502e8
pierreozoux <pierre.ozoux@seedrs.com>
2013-08-05 15:59:27 +0100

    bump to 1.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pierre.ozoux@gmail.com'
 license          'WTFPL'
 description      'Installs/Configures jenkins_github-hook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "jenkins"
 depends "nginx"
